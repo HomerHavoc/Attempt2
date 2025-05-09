384a159e18f17bd224a2945f04c7a1d37f906210
HomerHavoc <b9kmegaways@gmail.com>
2025-05-09 01:18:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ from apex_engine.main_engine import run_daily_predictions
 
 if __name__ == '__main__':
     run_daily_predictions()
+        - name: Export Predictions to CSV
+      run: |
+        python export_predictions.py
